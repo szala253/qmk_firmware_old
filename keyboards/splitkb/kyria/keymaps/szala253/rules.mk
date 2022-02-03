15303310d09a7894fb86378e297eade4dae42536
Gergo Szalai <gergo.szalai15@gmail.com>
2022-02-03 13:47:31 +0100

    add my own keymap

---
+++ keyboards/splitkb/kyria/keymaps/szala253/rules.mk
@@ -0,0 +1,7 @@
+OLED_ENABLE = no
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+
+UNICODEMAP_ENABLE = yes
+
+UNICODE_ENABLE = no
